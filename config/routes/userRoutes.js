ed654e38c31c7395583e3dad757419ad0f5da149
Lauren <llwor94@gmail.com>
2018-12-19 23:52:28 -0800

    updated user get request for query

--- config/routes/userRoutes.js
+++ config/routes/userRoutes.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/dbConfig');
 
-router.get('/', (req, res, next) => {
-	db('users')
-		.select('email', 'username', 'id', 'img_url')
-		.then(users => res.status(200).json(users))
-		.catch(next);
+router.get('/', ({ query }, res, next) => {
+	if (query.username) {
+		db('users')
+			.where({ username: query.username })
+			.select('email', 'username', 'id', 'img_url')
+			.first()
+			.then(user => res.status(200).json(user))
+			.catch(next);
+	} else {
+		db('users')
+			.select('email', 'username', 'id', 'img_url')
+			.then(users => res.status(200).json(users))
+			.catch(next);
+	}
 });
 
 module.exports = router;
